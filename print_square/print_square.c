387cee90a749757ddd87879c2c12b94d05962feb
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-20 20:16:11 +0000

    square done

--- print_square/print_square.c
+++ print_square/print_square.c
@@ -1,6 +1,39 @@
 int print_char(char);
+void print_top_or_bottom(int);
 
 void print_square(int n)
 {
-  print_char(n+48);
+  int i;
+  int j;
+
+  if (n == 1)
+    {
+      print_char('o');
+      print_char('\n');
+    }
+
+  else if (n > 1)
+    {
+      print_top_or_bottom(n);
+      for (j = 2; j < n; j++)
+	{
+	  print_char('|');
+	  for (i = 2; i < n; i++)
+	    print_char(' ');
+	  print_char('|');
+	  print_char('\n');
+	}
+      print_top_or_bottom(n);
+    }
+}
+
+void print_top_or_bottom(int n)
+{
+  int i;
+
+  print_char('o');
+  for (i = 2; i < n; i++)
+    print_char('-');
+  print_char('o');
+  print_char('\n');
 }
